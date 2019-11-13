b452cf7963e0f4c779bd15ca889a741c2004e6be
Dale Seo <dale.seo@gmail.com>
2019-11-12 20:28:19 -0500

    Set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ["airbnb-base"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/*.test.js"] }
+    ]
+  }
+};
