15f4f83dbd3ec20017deea4620f6d2b18d7b348a
Dale Seo <dale.seo@gmail.com>
2019-11-13 22:00:44 -0500

    Option to exempt branches fix #5

---
+++ src/github/filterStaleBranches.test.js
@@ -0,0 +1,66 @@
+const filterStaleBranches = require('./filterStaleBranches');
+const axios = require('axios');
+
+jest.mock('axios');
+
+test('filterStaleBranches() excludes specifed branches', async () => {
+    // given
+    const past = new Date();
+    past.setMonth(past.getMonth() - 4);
+
+    const nodes = ['bugfix/foo', 'bugfix/bar', 'develop', 'feature/foo', 'feature/bar', 'master']
+        .map(name => ({
+            name: name,
+            target: {
+              committedDate: past.toISOString(),
+              author: { user: {} }
+            }
+        }));
+    const excludedBranches = ['master', 'develop'];
+
+    // when
+    const branches = filterStaleBranches({ nodes, excludedBranches });
+
+    // then
+    expect(branches.map(({ name }) => name)).toEqual(
+        expect.not.arrayContaining(excludedBranches)
+    );
+});
+
+test('filterStaleBranches() filter by committed date', async () => {
+    // given
+    const past = new Date();
+    past.setMonth(past.getMonth() - 4);
+
+    const staleBranches = ['stale-1', 'stale-2'];
+    const freshBranches = ['fresh-1', 'fresh-2', 'fresh-3']
+
+    const nodes = [
+        ...staleBranches.map(name => ({
+            name: name,
+            target: {
+              committedDate: past.toISOString(),
+              author: { user: {} }
+            }
+        })),
+        ...freshBranches.map(name => ({
+            name: name,
+            target: {
+              committedDate: new Date().toISOString(),
+              author: { user: {} }
+            }
+        }))
+    ]
+
+    // when
+    const branches = filterStaleBranches({ nodes });
+
+    // then
+    expect(branches.map(({ name }) => name)).toEqual(
+        expect.arrayContaining(staleBranches)
+    );
+
+    expect(branches.map(({ name }) => name)).toEqual(
+        expect.not.arrayContaining(freshBranches)
+    );
+});
