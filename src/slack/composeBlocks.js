2f8a6247a625b6a5135c33701aa43ac32d02a07f
Dale Seo <dale.seo@gmail.com>
2019-11-28 10:46:02 -0500

    mssing return

--- src/slack/composeBlocks.js
+++ src/slack/composeBlocks.js
@@ -1,5 +1,5 @@
 module.exports = ({ repository, branches }) => {
-  branches.map(({ name, date, user }) => {
+  return branches.map(({ name, date, user }) => {
     const link = `>*<https://github.com/SMARTeacher/${repository}/tree/${name}|${name}>*`;
     const meta = `>:alarm_clock: _${date}_ :ed: _${user.name || user.login || user.email}_`;
     return {
