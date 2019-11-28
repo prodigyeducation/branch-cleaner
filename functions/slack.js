4a1cc59099a6f2298755f80e7871ce21ccce56c3
Chris Richard <christopher.richard@prodigygame.com>
2019-11-28 13:33:36 -0500

    uncomment core functionality

--- functions/slack.js
+++ functions/slack.js
@@ -9,36 +9,36 @@ exports.handler = async (event) => {
   if (!isAuthorized(event)) {
     return { statusCode: 403, body: 'Verrification failed'};
   };
-  //
-  // const { channel, message, user, actions } = JSON.parse(
-  //   decodeURIComponent(event.body).substring(8),
-  // );
-  //
-  // const tokens = actions[0].value.split('|');
-  //
-  // if (tokens.length === 3) {
-  //   const [owner, repository, branch] = tokens;
-  //   await deleteBranchThenUpdateMessage({
-  //     owner,
-  //     repository,
-  //     branch,
-  //     channel,
-  //     message,
-  //     user,
-  //   });
-  // } else {
-  //   const [repository, branch] = tokens;
-  //   await deleteBranchThenUpdateMessage({
-  //     repository,
-  //     branch,
-  //     channel,
-  //     message,
-  //     user,
-  //   });
-  // }
-  //
-  // return {
-  //   statusCode: 200,
-  //   body: event.body,
-  // };
+  
+  const { channel, message, user, actions } = JSON.parse(
+    decodeURIComponent(event.body).substring(8),
+  );
+
+  const tokens = actions[0].value.split('|');
+
+  if (tokens.length === 3) {
+    const [owner, repository, branch] = tokens;
+    await deleteBranchThenUpdateMessage({
+      owner,
+      repository,
+      branch,
+      channel,
+      message,
+      user,
+    });
+  } else {
+    const [repository, branch] = tokens;
+    await deleteBranchThenUpdateMessage({
+      repository,
+      branch,
+      channel,
+      message,
+      user,
+    });
+  }
+
+  return {
+    statusCode: 200,
+    body: event.body,
+  };
 };
