2612e1bed50b1487857eb55700f59e7202776c12
Dale Seo <dale.seo@gmail.com>
2019-11-27 23:33:49 -0500

    Set up Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,19 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    jest: true
+    jest: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   rules: {
-    "import/no-extraneous-dependencies": [
-      "error",
-      { devDependencies: ["**/*.test.js"] }
-    ]
-  }
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.js'] }],
+  },
 };
