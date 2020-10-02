d25df99d7dfc42ee4bf47d61d1595ba4c8d9a7b4
Christine Yu <christine.yu@prodigygame.com>
2020-10-02 15:55:18 -0400

    Celebration message for 0 stale branches

--- src/slack/publishMessages.js
+++ src/slack/publishMessages.js
@@ -1,10 +1,17 @@
 const { postMessage } = require('./chat');
 const composeBlocks = require('./composeBlocks');
 
-module.exports = async ({ channel, repository, branches }) => {
+const normalHeaderMessage = ({ branches, repository }) =>
+  `<!here> There are *${branches.length}* stale branches in *${repository}*! Click the delete button if the branch is no longer used. :do_not_litter:`;
+const zeroStaleHeaderMessage = ({ repository }) =>
+  `🎊 There are *ZERO* stale branches in *${repository}*!👏`;
+
+const defaultFunction = async ({ channel, repository, branches }) => {
   const header = {
     type: 'mrkdwn',
-    text: `<!here> There are *${branches.length}* stale branches in *${repository}*! Click the delete button if the branch is no longer used. :do_not_litter:`,
+    text: branches.length
+      ? normalHeaderMessage({ branches, repository })
+      : zeroStaleHeaderMessage({ repository }),
   };
 
   const headerBlocks = [
@@ -25,11 +32,11 @@ module.exports = async ({ channel, repository, branches }) => {
 
   const blocks = composeBlocks({ repository, branches });
 
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < blocks.length; i++) {
-    // eslint-disable-next-line no-await-in-loop
-    await postMessage({ channel, blocks: [blocks[i]] });
-  }
+  blocks.forEach((block) => postMessage({ channel, blocks: [block] }));
 
   return response;
 };
+
+module.exports = defaultFunction;
+module.exports.normalHeaderMessage = normalHeaderMessage;
+module.exports.zeroStaleHeaderMessage = zeroStaleHeaderMessage;
